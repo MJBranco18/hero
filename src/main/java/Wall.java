fb4cd20c3eed9a4842b98c8a63d73d29d3e9ae38
MJBranco18 <mario.branco18@gmail.com>
2022-10-12 19:04:16 +0100

    Added Class Element

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -3,20 +3,15 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-public class Wall {
-    private Position position;
+    public class Wall extends Element{
 
-    public Wall(int x, int y){
-        this.position = new Position(x,y);
-    }
+        public Wall(int x, int y) {
+            super(x, y);
+        }
 
-    public void draw(TextGraphics graphics) {
-        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
-        graphics.enableModifiers(SGR.BOLD);
-        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
+        public void draw(TextGraphics graphics) {
+            graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+            graphics.enableModifiers(SGR.BOLD);
+            graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
+        }
     }
-
-    public Position getPosition() {
-        return position;
-    }
-}
